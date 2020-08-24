fa0f7576df3e49145ff15f92e4e512b8eef50268
Liz Glavin <eglavin@gmail.com>
2020-08-23 15:41:46 -1000

    Updates

---
+++ precache-manifest.85eced9f3ff6c002bd0146322d90fd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8594c0ee6a5cee1843c585b44aa9a849",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c2c0bff29df1b916ec18",
+    "url": "/monsters-rolodex/static/css/main.c8c272b8.chunk.css"
+  },
+  {
+    "revision": "a324381d0b528c37116c",
+    "url": "/monsters-rolodex/static/js/2.88478b8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.88478b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c0bff29df1b916ec18",
+    "url": "/monsters-rolodex/static/js/main.9682a5ad.chunk.js"
+  },
+  {
+    "revision": "676fa6fe07c881f9e750",
+    "url": "/monsters-rolodex/static/js/runtime-main.9c224c16.js"
+  }
+]);
